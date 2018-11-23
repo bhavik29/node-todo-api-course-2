9f2706792a107c2c6d66e2a3cd6f9616dae6117d
Bhavik Thakkar <bhavik.t@lucideustech.com>
2018-11-23 23:58:13 +0530

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new ToDo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   })
 
   todo.save().then((doc) => {
@@ -28,22 +29,27 @@ app.post('/todos', (req, res) => {
   })
 });
 
-app.get('/todos', (req, res) => {
-  ToDo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  ToDo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   })
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   };
 
-  ToDo.findById(id).then((todo) => {
+  ToDo.findOne({
+    _id: id,
+    _creator: req.user._id
+    }).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
@@ -53,14 +59,17 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
 
-  ToDo.findByIdAndRemove(id).then((todo) => {
+  ToDo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if(!todo){
       return res.status(404).send();
     };
@@ -71,7 +80,7 @@ app.delete('/todos/:id', (req, res) => {
   })
 })
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -86,7 +95,10 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  ToDo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  ToDo.findOneAndUpdate({
+      _id: id,
+      _creator: req.user._id
+    }, {$set: body}, {new: true}).then((todo) => {
     if(!todo){
       res.status(404).send();
     }
